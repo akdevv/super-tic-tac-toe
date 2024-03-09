ee52aab936c91c69fe3b778b3149d5764fb318f1
Ashish Kumar <akdev5520@gmail.com>
2024-03-09 14:52:45 +0530

    added ActiveGrid logic

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -3,17 +3,37 @@ import { useState } from "react";
 
 function GameBoard() {
 	const [xIsNext, setXIsNext] = useState(true);
+	const [activeGrid, setActiveGrid] = useState(null);
 	const [squares, setSquares] = useState(
 		[...Array(9)].map(() => Array(9).fill(null))
 	);
 
-	const handleClick = (i, j) => {
+	const isGridFull = (gridIndex) => {
+		return squares[gridIndex].every((value) => value !== null);
+	};
+
+	const gameLogic = (i, j) => {
 		if (squares[i][j]) return;
+
 		const nextSquares = [...squares];
 		nextSquares[i] = [...nextSquares[i]];
 		nextSquares[i][j] = xIsNext ? "X" : "O";
+
+		const nextActiveGrid = j;
+
+		if (isGridFull(nextActiveGrid)) {
+			setActiveGrid(null); // Set all grids as active for one move
+		} else {
+			setActiveGrid(nextActiveGrid);
+		}
+
 		setSquares(nextSquares);
 		setXIsNext(!xIsNext);
+	};
+
+	const handleClick = (i, j) => {
+		if (activeGrid && activeGrid !== i && !isGridFull(activeGrid)) return;
+		gameLogic(i, j);
 
 		console.log("i", i, "j", j);
 	};
@@ -27,7 +47,7 @@ function GameBoard() {
 	);
 
 	const renderRow = (row, i) => (
-		<div className="inline-grid grid-cols-3 gap-1">
+		<div key={i} className="inline-grid grid-cols-3 gap-1">
 			{row.map((value, j) => renderSquare(value, i, j))}
 		</div>
 	);
@@ -35,8 +55,9 @@ function GameBoard() {
 	return (
 		<>
 			<h1>Game Board</h1>
-			<div className="inline-grid grid-cols-3 gap-3">
+			<div className="inline-grid grid-cols-3 gap-3 m-3">
 				{squares.map((row, i) => renderRow(row, i))}
+				ActiveGrid: {activeGrid}
 			</div>
 		</>
 	);
