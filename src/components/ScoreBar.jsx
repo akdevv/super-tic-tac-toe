c8c524a25b19d34493baca955cadbe5693b0b299
Ashish Kumar <akdev5520@gmail.com>
2024-03-11 18:25:18 +0530

    added responsive styles to score bar

--- src/components/ScoreBar.jsx
+++ src/components/ScoreBar.jsx
@@ -1,22 +1,21 @@
-import React from "react";
 import { NameChipBlue, NameChipRed } from "./NameChip";
 
 function ScoreBar() {
-	const scoreCells = Array(9).fill(0);
+	// const scoreCells = Array(9).fill(0);
 	return (
-		<div className="flex">
+		<div className="flex flex-col w-full items-center">
 			{/* names components */}
-			<div>
+			<div className="flex sm:w-3/5 md:w-3/4 w-full mb-1.5 justify-between">
 				<NameChipRed name={"Player1"} />
 				<NameChipBlue name={"Player2"} />
 			</div>
 			{/* score bar component */}
-			<div className="flex w-3/4 h-2 rounded-full bg-primaryGray">
-				{scoreCells.map((_, idx) => (
+			<div className="flex sm:w-3/5 md:w-3/4 w-full h-2 rounded-full bg-primaryGray">
+				{/* {scoreCells.map((_, idx) => (
 					<React.Fragment key={idx}>
 						{idx === 0 && <div></div>}
 					</React.Fragment>
-				))}
+				))} */}
 			</div>
 		</div>
 	);
