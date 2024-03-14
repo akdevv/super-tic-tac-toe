2e53ebd39b7837f19a5fba1b5f0864bde8386fa6
Ashish Kumar <akdev5520@gmail.com>
2024-03-14 23:19:14 +0530

    optimised getGridWinner method

--- src/utils/helpers/getGridWinner.js
+++ src/utils/helpers/getGridWinner.js
@@ -2,7 +2,19 @@
  * Finds and returns the winner in a tic-tac-toe grid.
  *
  * @param {Array<string|null>} squares The tic-tac-toe grid to check for a winner.
+ *
  * @returns {?{winner: string, sequence: number[]}} Object with winner and sequence | null.
+ *
+ * @example
+ * // Example: Winner "X" in the top row
+ * const squares = ["X", "X", "X", "O", null, "O", null, null, null];
+ * getGridWinner(squares); // Returns { winner: "X", sequence: [0, 1, 2] }
+ *
+ * @example
+ * // Example: No winner
+ * const squares = ["X", "O", "X", "X", "O", "O", "O", "X", "X"];
+ * getGridWinner(squares); // Returns null
+ *
  */
 
 const getGridWinner = (squares) => {
@@ -31,8 +43,8 @@ const getGridWinner = (squares) => {
 		) {
 			return { winner: squares[a], sequence: line };
 		}
-		return null;
 	}
+	return null;
 };
 
 export default getGridWinner;
