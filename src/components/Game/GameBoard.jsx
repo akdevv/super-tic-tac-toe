6b5ff034fbc6fb19726d98a25b8582a41f635309
Ashish Kumar <akdev5520@gmail.com>
2024-03-18 09:06:37 +0530

    moved renderGrid component into seprate file

--- src/components/Game/GameBoard.jsx
+++ src/components/Game/GameBoard.jsx
@@ -1,60 +1,9 @@
 import updateGameStates from "../../utils/helpers/updateGameStates";
 import getGridWinner from "../../utils/helpers/getGridWinner";
 import useGameLogic from "../../hooks/useGameLogic";
-import { getWinningLineStyles } from "../../utils/gameFunctions";
-import NameChip from "../NameChip";
-import renderCell from "../../utils/renderCell";
+import renderGrid from "../../utils/renderGrid";
 
 function GameBoard(props) {
-	// renders individual tic-tac-toes
-	const renderGrid = (row, i, activeGrid, wonGrids, handleClick) => {
-		// winner line style
-		const lineStyle = getWinningLineStyles(wonGrids, i);
-
-		return (
-			<div
-				key={i}
-				className={`relative inline-grid grid-cols-3 gap-0.5 bg-primaryGray border-4 group ${
-					activeGrid === i
-						? "border-playerBlue"
-						: "border-primaryLight"
-				}`}
-			>
-				{row.map((value, j) => renderCell(value, i, j, handleClick))}
-
-				{wonGrids[i] && (
-					<>
-						<div
-							className="absolute h-0.5 bg-primaryDark"
-							style={{ ...lineStyle }}
-						/>
-						<div
-							className="absolute invisible transition-all duration-100 ease-in-out bg-center group-hover:visible"
-							style={{
-								top: "50%",
-								left: "50%",
-								transform: "translate(-50%, -50%)",
-							}}
-						>
-							{wonGrids[i].result === "W" && (
-								<NameChip
-									name={"Player1"}
-									color={"playerRed"}
-								/>
-							)}
-							{wonGrids[i].result === "L" && (
-								<NameChip
-									name={"Player2"}
-									color={"playerBlue"}
-								/>
-							)}
-						</div>
-					</>
-				)}
-			</div>
-		);
-	};
-
 	const {
 		xIsNext,
 		setXIsNext,
