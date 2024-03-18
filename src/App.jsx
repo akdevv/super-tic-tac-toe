8c507cf1b7afa79b130598ed4486186a97d88e57
Ashish Kumar <akdev5520@gmail.com>
2024-03-18 07:58:29 +0530

    moved Info & Settings Btn into single component

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,16 @@
-import { useState } from "react";
 import Footer from "./components/Footer";
+import Button from "./components/Button";
 import Game from "./components/Game/Game";
-import InfoBtn from "./components/InfoBtn";
-import SettingsBtn from "./components/SettingsBtn";
 
 function App() {
-	const [showInfo, setShowInfo] = useState(false);
-
-	const toggleInfoCard = () => {
-		setShowInfo(!showInfo);
-	};
-
 	return (
 		<>
-			<div
-				className={`flex flex-col justify-center h-screen p-5 bg-primaryLight`}
-			>
+			<div className="flex flex-col justify-center h-screen p-5 bg-primaryLight">
 				<Game />
 				<div className="flex justify-between">
-					<SettingsBtn />
+					<Button src="/settings-btn.svg" />
 					<Footer />
-					<InfoBtn
-						text={"i"}
-						showInfo={showInfo}
-						onClick={toggleInfoCard}
-					/>
+					<Button text={"i"} />
 				</div>
 			</div>
 		</>
