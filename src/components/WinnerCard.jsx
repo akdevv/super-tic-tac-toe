0bc78a02f982b9c9b5e0baf24e7b0af0f8244942
Ashish Kumar <akdev5520@gmail.com>
2024-03-30 21:54:05 +0530

    updated WinnerCard styles

--- src/components/WinnerCard.jsx
+++ src/components/WinnerCard.jsx
@@ -1,7 +1,32 @@
 function WinnerBanner({ winner }) {
+	let color;
+	let winnerName;
+	if (winner === "L") {
+		color = "bg-playerRed";
+		winnerName = "Player 1";
+	} else if (winner === "W") {
+		color = "bg-playerBlue";
+		winnerName = "Player 2";
+	} else {
+		color = "bg-primaryGray";
+		winnerName = "Nooo one";
+	}
 	return (
-		<div>
-			<h1>{winner} wins!</h1>
+		<div className="absolute w-full h-full backdrop-blur-sm bg-primaryLight/30">
+			<div className="flex items-center justify-center h-dvh">
+				<div
+					className={`flex items-center justify-center gap-2 px-2 py-2 rounded-full w-52 font-firaMono ${color}`}
+				>
+					<p className="font-medium">{winnerName} wins!</p>
+					<button>
+						<img
+							src="/restart-btn.svg"
+							alt="restart button"
+							className="w-12"
+						/>
+					</button>
+				</div>
+			</div>
 		</div>
 	);
 }
